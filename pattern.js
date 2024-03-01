9cdbd72172477f017706292150b4a1d5779b8402
Prakhar-vaid24 <prakharvaid5519@gmail.com>
2024-03-01 12:52:37 +0530

    Adding project files

---
+++ pattern.js
@@ -0,0 +1,20 @@
+'use strict';
+//document.write('h')
+let string = '', rows = 5;
+const square = () => {}
+
+//square()
+
+const lefttriangle = () => {}
+const leftreversetriangle = () => {}
+const righttriangle = () => {}
+const rightreversetriangle = () => {}
+const pyramid = () => {}
+const reversepyramid = () => {}
+const Diamond = () => {}
+const hourglass = () => {}
+const ASCIICodeToAlpha = () => {}
+const hollowsquare = () => {}
+const hollowtriangle = () => {}
+const hollowpyramid = () => {}
+const hollowDiamond = () => {}
